8a5fd92208597f68aad1249727501ff46a5b49c9
hoang1998zl <hhoaang@gmail.com>
2023-12-03 23:48:19 +0700

    update

--- src/Components/Login/SignupForm.jsx
+++ src/Components/Login/SignupForm.jsx
@@ -68,6 +68,36 @@ const SignupForm = ({
           onChange={(e) => setEmail(e.target.value)}
         />
       </div>
+      <div className="mb-6">
+        <label
+          htmlFor="signup_name"
+          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        >
+          Name:
+        </label>
+        <input
+          type="text"
+          id="signup_name"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 focus:outline-none"
+          required
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+      <div className="mb-6">
+        <label
+          htmlFor="signup_phone"
+          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        >
+          Phone:
+        </label>
+        <input
+          type="text"
+          id="signup_phone"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 focus:outline-none"
+          required
+          onChange={(e) => setPhoneNumber(e.target.value)}
+        />
+      </div>
       <div className="mb-6 relative">
         <label
           htmlFor="signup_password"
@@ -131,36 +161,6 @@ const SignupForm = ({
               <i className='fa-solid fa-eye'></i>
           }
         </button>
-      </div>
-      <div className="mb-6">
-        <label
-          htmlFor="signup_name"
-          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-        >
-          name:
-        </label>
-        <input
-          type="text"
-          id="signup_name"
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 focus:outline-none"
-          required
-          onChange={(e) => setName(e.target.value)}
-        />
-      </div>
-      <div className="mb-6">
-        <label
-          htmlFor="signup_phone"
-          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-        >
-          Phone:
-        </label>
-        <input
-          type="text"
-          id="signup_phone"
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 focus:outline-none"
-          required
-          onChange={(e) => setPhoneNumber(e.target.value)}
-        />
       </div>
       <div
         className="flex justify-between mb-6"
